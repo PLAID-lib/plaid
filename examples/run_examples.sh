ab9d050fcb4fe78c07fca48df6c487e77738d3ef
Alex DR <alexandre.devaux-riviere@safrangroup.com>
2023-11-15 10:30:36 +0100

    (pipeline will fail) fix(bissect.py, run_examples.sh): fix error detection in example stage for pipeline

--- examples/run_examples.sh
+++ examples/run_examples.sh
@@ -3,4 +3,7 @@ for file in *.py utils/*.py containers/*.py plot/*.py
     do echo "--------------------------------------------------------------------------------------"
         echo "#---# run python $file"
         python $file
+        if [ $? -ne 0 ]; then
+            exit 1
+        fi
     done
