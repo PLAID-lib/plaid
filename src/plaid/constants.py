a114a6f649d8fab0ca559f64b273b53fd0f8ff10
Brian Staber <brian.staber@gmail.com>
2025-06-30 22:58:03 +0200

    missing license text

--- src/plaid/constants.py
+++ src/plaid/constants.py
@@ -1,5 +1,12 @@
 """Common constants used across the PLAID library."""
 
+# -*- coding: utf-8 -*-
+#
+# This file is subject to the terms and conditions defined in
+# file 'LICENSE.txt', which is part of this source code package.
+#
+#
+
 from typing import Literal
 
 AUTHORIZED_TASKS = Literal["regression", "classification"]
