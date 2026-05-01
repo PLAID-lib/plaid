9e65b1c3d8dbdd18095deeb4d624b19186f7f50b
Felipe Bordeu <felipe.bordeu@ssg-aero.com>
2026-05-01 11:49:55 +0930

    Delete pipelines

--- src/plaid/pipelines/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Package for PLAID containers such as `Dataset` and `Sample`."""
-
-# -*- coding: utf-8 -*-
-#
-# This file is subject to the terms and conditions defined in
-# file 'LICENSE.txt', which is part of this source code package.
-#
-#
